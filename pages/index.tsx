aacc9d624405d37c6da369d7661eb8fa829db952
YBK814 <romandawn0814@gmail.com>
2023-10-12 23:58:54 +0900

    [커밋] 두번째

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,12 @@
 import Link from 'next/link'
 import Layout from '../components/Layout'
+import Button from '../components/Button'
+import styles from "../pages/App.module.css"
 
 const IndexPage = () => (
   <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
+    <h1 className={styles.title}>Hello Next.js 👋</h1>
+    <Button text={'Continue'} />
     <p>
       <Link href="/about">About</Link>
     </p>
